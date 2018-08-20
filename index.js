db744b9ef869bd9efa1695f4f720bc70803208e7
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-20 11:06:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,21 +7,5 @@ function BoardMember (name, homeState, training) {
     return "No, I must disagree";
   }
 
-  this.approve = function() {
-    return "You can do that!";
-  }
-
-  this.doCharity = function() {
-    return "You can do that!";
-  }
-
-  this.releasePressStatement  = function() {
-    return "You will see great things from Scuber.You can do that!";
-  }
-
-  this.sayHi  = function() {
-    console.log( `Hi, my name is ${this.name}. I am from ${this.homeTown}")
-  }
-
 
 }
