3af2ed6ec842af2a2200df81eda93c0ebd408f12
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-20 11:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function BoardMember (name, homeState, training) {
   }
 
   this.sayHi  = function() {
-    return  `Hi, my name is ${this.name}. I am from ${this.homeTown}`
+    return  `Hi, my name is ${this.name}. I am from ${this.homeTown}, and I was trained in ${this.training}.`
   }
 
 }
