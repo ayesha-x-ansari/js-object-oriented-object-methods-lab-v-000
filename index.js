937adf1ab00064ec0b2b9da4af23d754f5226721
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-20 11:01:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ function BoardMember (name, homeState, training) {
   this.name = name
   this.homeState = homeState
   this.training = training
-}
+
+  this.veto = function(){
+    console.log("No, I must disagree");
+  }
+
+  this approve = function() {
+    console.log("You can do that!");
+  }
+
+  this doCharity = function() {
+    console.log("You can do that!");
+  }
+
+  this releasePressStatement  = function() {
+    console.log("You will see great things from Scuber.You can do that!");
+  }
+
+  this sayHi  = function() {
+    console.log( `Hi, my name is ${this.name}. I am from ${this.homeTown}")
+  }
