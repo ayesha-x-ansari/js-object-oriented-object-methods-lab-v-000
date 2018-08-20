b3e9c7c7459b63237bdfacaa680981aaf5080bc4
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-20 11:10:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function BoardMember (name, homeState, training) {
   }
 
   this.releasePressStatement  = function() {
-    return "You will see great things from Scuber.You can do that!";
+    return "AssertionError: expected 'You will see great things from Scuber.You can do that!' to equal 'You will see great things from Scuber.";
   }
 /*
   this.sayHi  = function() {
